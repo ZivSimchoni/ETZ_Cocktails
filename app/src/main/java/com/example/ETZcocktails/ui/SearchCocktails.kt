8794d28cb546f6371d11987d524875a2af27afe1
Ziv Simchoni <zivi4444@gmail.com>
2023-01-31 18:53:54 +0200

    Remove unused imports, add bindings, XML text input
    Add:
    + Small and stupid Input Validation
    + Changes to UI
    + Add TODOs

--- app/src/main/java/com/example/ETZcocktails/ui/SearchCocktails.kt
+++ app/src/main/java/com/example/ETZcocktails/ui/SearchCocktails.kt
@@ -4,33 +4,21 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
-import android.widget.EditText
 import androidx.fragment.app.Fragment
-import com.example.ETZcocktails.Cocktail
 import com.example.ETZcocktails.CocktailList
-import com.example.ETZcocktails.R
 import com.example.ETZcocktails.data.models.RetrofitHelper
-import com.example.ETZcocktails.utils.autoCleared
+import com.example.ETZcocktails.databinding.FragmentSearchCocktailsBinding
 import com.example.ETZcocktails.ui.all_characters.CocktailAdapter
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
 
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-
-/**
- * A simple [Fragment] subclass.
- * Use the [SearchCocktails.newInstance] factory method to
- * create an instance of this fragment.
- */
 class SearchCocktails : Fragment() {
-    private var binding : SearchCocktails by autoCleared()
+
+    private var _binding : FragmentSearchCocktailsBinding? = null
+
+    private val binding get() = _binding!!
 
     //private val viewModel: cocktail_view_list by viewModels()
 
@@ -38,28 +26,29 @@ class SearchCocktails : Fragment() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-        arguments?.let {
-        }
-
-
     }
 
-    fun getCocktails(CockToSearch :String){
+    fun GetCocktailsByName(CockToSearch :String){
+        // TODO: re-implement this function - null safety
         val API = RetrofitHelper.FetchCocktailByName(CockToSearch)
         API?.enqueue(object: Callback<CocktailList?> {
             override fun onResponse(
                 call: Call<CocktailList?>,
                 response: Response<CocktailList?>
             ) {
-                if (response.body() != null) {
+                if (response.body()?.drinks != null) {
                     val cocktailList = response.body()!!.drinks!!
                     //printCocktails(cocktailList) // error so comment
                     for (cocktail in cocktailList) {
                         println(cocktail.strDrink)
                     }
                 }
+                else
+                {
+                    println("error Drinks NULL!")
+                }
             }
+
             override fun onFailure(call: Call<CocktailList?>, t: Throwable) {
                     TODO("Not yet implemented")
                 println("error")
@@ -78,33 +67,13 @@ class SearchCocktails : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        val view: View = inflater!!.inflate(R.layout.fragment_search_cocktails, container, false)
 
-        view.findViewById<Button>(R.id.searchButton).setOnClickListener {//Search for cocktails
-            getCocktails(view.findViewById<EditText>(R.id.CocktailToSearchInput).text.toString())
+        _binding = FragmentSearchCocktailsBinding.inflate(inflater,container,false)
+        // TODO InputValidation
+        binding.searchButton.setOnClickListener {
+            GetCocktailsByName(binding.CocktailToSearchInput.text.toString())
         }
 
-        return view
-    }
-
-    companion object{
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment SearchCocktails.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            SearchCocktails().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+        return binding.root
     }
 }
--- app/src/main/java/com/example/ETZcocktails/ui/SearchIngredient.kt
+++ app/src/main/java/com/example/ETZcocktails/ui/SearchIngredient.kt
@@ -5,18 +5,13 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
-import android.widget.EditText
 import com.example.ETZcocktails.CocktailList
 import com.example.ETZcocktails.R
 import com.example.ETZcocktails.data.models.RetrofitHelper
-import com.example.ETZcocktails.databinding.FragmentAddCocktailBinding
 import com.example.ETZcocktails.databinding.FragmentSearchIngredientBinding
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
-import com.example.ETZcocktails.utils.autoCleared
-import com.example.ETZcocktails.ui.all_characters.CocktailAdapter
 
 
 class SearchIngredient : Fragment() {
@@ -34,10 +29,9 @@ class SearchIngredient : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        _binding = FragmentSearchIngredientBinding.inflate(inflater,container,false)
-        //val view: View = inflater.inflate(R.layout.fragment_search_ingredient, container, false)
 
+        _binding = FragmentSearchIngredientBinding.inflate(inflater,container,false)
+        // TODO InputValidation
         binding.searchButton.setOnClickListener {
             GetCocktailsByIng(binding.enterIngName.text.toString())
         }
@@ -46,6 +40,7 @@ class SearchIngredient : Fragment() {
     }
 
     fun GetCocktailsByIng(IngToSearch :String){
+        // TODO: re-implement this function - null safety
         val API = RetrofitHelper.FetchCocktailByIng(IngToSearch)
         API?.enqueue(object: Callback<CocktailList?> {
             override fun onResponse(
@@ -60,7 +55,7 @@ class SearchIngredient : Fragment() {
                 }
             }
             override fun onFailure(call: Call<CocktailList?>, t: Throwable) {
-                TODO("Not yet implemented")
+                // TODO("Not yet implemented")
                 println("error")
             }
         })
