5da9d7f1e25e7e9004af52756a433298dabfa2b7
Ziv Simchoni <zivi4444@gmail.com>
2023-02-11 15:00:56 +0200

    Update Strings, Double press to exit & Cocktail instructions

--- app/src/main/java/com/example/ETZcocktails/MainActivity.kt
+++ app/src/main/java/com/example/ETZcocktails/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.example.ETZcocktails
 
 import android.os.Bundle
+import android.os.Handler
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
@@ -57,20 +59,20 @@ class MainActivity : AppCompatActivity() {
     }
 
     // double click to exit -> the only downside that its over all the fragments
-//    private var doubleBackToExitPressedOnce = false
-//
-//    override fun onBackPressed() {
-//        if (doubleBackToExitPressedOnce) {
-//            super.onBackPressed()
-//            return
-//        }
-//        this.doubleBackToExitPressedOnce = true
-//        Toast.makeText(this,
-//            R.string.bye_message,
-//            Toast.LENGTH_SHORT
-//        ).show()
-//        Handler().postDelayed({ doubleBackToExitPressedOnce = false }, 2000)
-//    }
+    private var doubleBackToExitPressedOnce = false
+
+    override fun onBackPressed() {
+        if (doubleBackToExitPressedOnce) {
+            super.onBackPressed()
+            return
+        }
+        this.doubleBackToExitPressedOnce = true
+        Toast.makeText(this,
+            R.string.bye_message,
+            Toast.LENGTH_SHORT
+        ).show()
+        Handler().postDelayed({ doubleBackToExitPressedOnce = false }, 2000)
+    }
 
 
     private fun replaceFragment(fragment: Fragment) {
